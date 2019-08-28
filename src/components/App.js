d5c0e63b26f71e0b7d055a77467317501b253ab8
AhmadReza Zahedi <r.zahedi@gmail.com>
2019-08-28 18:15:46 +0430

    fresh start again

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      start again without any UI style this time!!!
+    </div>
+  );
+}
+
+export default App;
