810a103a8052742596148afc16bdb24a4c3e992e
AhmadReza Zahedi <r.zahedi@gmail.com>
2019-08-28 21:35:12 +0430

    test

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav/>
         <Route path='/' exact component={Dashboard} />
         <Route path='/new' component={NewQuestion} />
         <Route path='/leaderboard' component={Leaderboard} />
